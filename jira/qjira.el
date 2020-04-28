89d87a17bf73e36d4cd5e773a47a56e3639955ba
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2020-04-28 08:55:33 +0100

    1. No hanging closing parens! 2. No else branch? Prefer (when ...)

--- jira/qjira.el
+++ jira/qjira.el
@@ -13,36 +13,25 @@
 
 (defun jira-ticket-validation (string)
   "Take STRING an input and return jira ticket format if discovered."
-  (if (string-match "\\(\\([a-zA-Z]*\\)-[0-9]*\\)" string)
-      (let (
-	    (issue-number (match-string 1 string))
-	    (project (match-string 2 string))
-	    )
-	(if (and project issue-number)
-	    (if (car (member project '("IOS" "MAPI" "AND")))
-		issue-number
-	      )
-	  (message "Project description not detected")
-	  )
-	)
-    )
-  )
+  (when (string-match "\\(\\([a-zA-Z]*\\)-[0-9]*\\)" string)
+    (let ((issue-number (match-string 1 string))
+          (project (match-string 2 string)))
+      (if (and project issue-number)
+          (when (car (member project '("IOS" "MAPI" "AND"))) issue-number)
+        (message "Project description not detected")))))
+
 
 (defun jira-convert-to-link-with-sumary ()
   "Take current a symbol at point pointer and look up in order url, symbol (pasteboard?)."
   (interactive)
-  (if (thing-at-point 'symbol)
-      (let ((ticket (jira-ticket-validation (thing-at-point 'symbol))))
-	(if ticket
-	    (let ((summary (alist-get 'summary (alist-get 'fields (jiralib2-get-issue ticket)))))
-	      (message "Found ticket: %s %s" ticket summary)
-              (delete-region (car (bounds-of-thing-at-point 'symbol)) (cdr (bounds-of-thing-at-point 'symbol)))
-              (insert (concat (replace-regexp-in-string "\\[.*\\][ ]*" "" summary) " [[" jiralib2-url "/browse/" ticket "][" ticket "]]"))
-              )
-	  )
-	)
-    )
-  )
+  (when (thing-at-point 'symbol)
+    (let ((ticket (jira-ticket-validation (thing-at-point 'symbol))))
+      (when ticket
+        (let ((summary (alist-get 'summary (alist-get 'fields (jiralib2-get-issue ticket)))))
+          (message "Found ticket: %s %s" ticket summary)
+          (delete-region (car (bounds-of-thing-at-point 'symbol)) (cdr (bounds-of-thing-at-point 'symbol)))
+          (insert (concat (replace-regexp-in-string "\\[.*\\][ ]*" "" summary) " [[" jiralib2-url "/browse/" ticket "][" ticket "]]")))))))
+
 
 (provide 'qjira)
 ;;; qjira.el ends here
